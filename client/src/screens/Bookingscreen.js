c1f8534729a0b6982ef0fc2fe95822be49addf09
FlamurMorina <fm41566@ubt-uni.net>
2021-07-28 21:48:35 +0200

    adding components (error,loading,success)

--- client/src/screens/Bookingscreen.js
+++ client/src/screens/Bookingscreen.js
@@ -1,5 +1,8 @@
 import React, { useState, useEffect } from 'react'
 import axios from "axios";
+import Loader from "../components/Loader";
+import Error from "../components/Error";
+
 function Bookingscreen({ match }) {
     const [room, setroom] = useState();
     const [loading, setloading] = useState(true);
@@ -23,46 +26,47 @@ function Bookingscreen({ match }) {
 
     return (
         <div className='m-5'>
-            {loading ? (<h1>Loading...</h1>) : error ? (<h1>Error...</h1>) : (<div>
-                <div className="row justify-content-center md-5 bs">
-                    <div className="col-md-6">
-                        <h1>{room.name}</h1>
-                        <img src={room.imgurls[0]} className="bigimg" />
-
-                    </div>
-
-                    <div className="col-md-6">
-                        <div style={{ textAlign: 'right' }}>
-
-                            <h1>Booking Details</h1>
-                            <hr />
-
-                            <b>
-                                <p>Name : </p>
-                                <p>From Date :</p>
-                                <p>To Date :</p>
-                                <p>Max Count : {room.maxcount}</p>
-                            </b>
+            {loading ?
+                (<Loader />) : room ? (<div>
+                    <div className="row justify-content-center md-5 bs">
+                        <div className="col-md-6">
+                            <h1>{room.name}</h1>
+                            <img src={room.imgurls[0]} className="bigimg" />
 
                         </div>
-                        <div style={{ textAlign: 'right' }}>
-                            <b>
-                                <h1>Amount</h1>
+
+                        <div className="col-md-6">
+                            <div style={{ textAlign: 'right' }}>
+
+                                <h1>Booking Details</h1>
                                 <hr />
-                                <p>Total days :</p>
-                                <p>Rent per day {room.rentperday}</p>
-                                <p>Total Amount</p>
-                            </b>
-                        </div>
-                        <div style={{ float: 'right' }}>
-                            <button className="btn btn-primary">Pay Now</button>
-                        </div>
 
+                                <b>
+                                    <p>Name : </p>
+                                    <p>From Date :</p>
+                                    <p>To Date :</p>
+                                    <p>Max Count : {room.maxcount}</p>
+                                </b>
+
+                            </div>
+                            <div style={{ textAlign: 'right' }}>
+                                <b>
+                                    <h1>Amount</h1>
+                                    <hr />
+                                    <p>Total days :</p>
+                                    <p>Rent per day {room.rentperday}</p>
+                                    <p>Total Amount</p>
+                                </b>
+                            </div>
+                            <div style={{ float: 'right' }}>
+                                <button className="btn btn-primary">Pay Now</button>
+                            </div>
+
+                        </div>
                     </div>
-                </div>
 
 
-            </div>)}
+                </div>) : (<Error />)}
         </div>
     )
 }
--- client/src/screens/Homescreen.js
+++ client/src/screens/Homescreen.js
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from 'react'
 import axios from "axios";
 import Room from '../components/Room';
+import Loader from 'react-spinners/HashLoader';
+import Error from "../components/Error";
 function Homescreen() {
 
     const [rooms, setrooms] = useState([])
@@ -24,19 +26,20 @@ function Homescreen() {
     }, [])
     return (
         <div className='container'>
-            <div className="row justify-content-center mt-5">
+            <div className="row justify-content-center mt-5 ">
                 {loading ?
-                    (<h1>Loading ...</h1>
-                    ) : error ? (<h1>Error</h1>) : (rooms.map(room => {
-
-                        return <div className="col-md-9 mt-2">
+                    (<Loader />
+                    ) : rooms.length > 1 ? (rooms.map((room) => {
+                        return <div className="col-md-9 mt-3">
                             <Room room={room} />
                         </div>
                     })
+                    ) : (
+                        <Error />
                     )}
             </div>
         </div>
     );
 }
 
-export default Homescreen
+export default Homescreen;
