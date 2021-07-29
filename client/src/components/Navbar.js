b8679ea8d744ed7a89b092c897df61934fbf43ec
FlamurMorina <fm41566@ubt-uni.net>
2021-07-29 20:25:41 +0200

    User authentication

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,22 +1,42 @@
 import React from 'react'
 
 function Navbar() {
+    const user = JSON.parse(localStorage.getItem('currentUser'));
+    function logout() {
+        localStorage.removeItem('currentUser')
+        window.location.href = '/login'
+    }
     return (
         <div>
             <nav class="navbar navbar-expand-lg">
                 <div class="container-fluid">
                     <a class="navbar-brand" href="#">Rooms</a>
                     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                        <span class="navbar-toggler-icon"></span>
+                        <span class="navbar-toggler-icon" ><i class="fa fa-bars" style={{ color: 'white' }}></i></span>
                     </button>
                     <div class="collapse navbar-collapse" id="navbarNav">
-                        <ul class="navbar-nav">
-                            <li class="nav-item">
-                                <a class="nav-link active" aria-current="page" href="/register">Register</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="/login">Login</a>
-                            </li>
+                        <ul class="navbar-nav mr-5">
+                            {user ? (
+                                <>
+                                    <div class="dropdown">
+                                        <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                            <i className='fa fa-user'></i>  {user.name}
+                                        </button>
+                                        <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                                            <a class="dropdown-item" href="#">Bookings</a>
+                                            <a class="dropdown-item" href="#" onClick={logout}>Logout</a>
+                                        </div>
+                                    </div>
+                                </>
+                            ) : (<>
+                                <li class="nav-item">
+                                    <a class="nav-link active" aria-current="page" href="/register">Register</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="/login">Login</a>
+                                </li>
+
+                            </>)}
                         </ul>
                     </div>
                 </div>
--- client/src/screens/Loginscreen.js
+++ client/src/screens/Loginscreen.js
@@ -1,11 +1,14 @@
 import React, { useState, useEffect } from 'react'
 import axios from "axios";
-
+import Loader from "../components/Loader";
+import Error from "../components/Error";
 
 
 function Loginscreen() {
     const [email, setemail] = useState('')
     const [password, setpassword] = useState('')
+    const [loading, setloading] = useState(false);
+    const [error, seterror] = useState();
 
     async function Login() {
 
@@ -14,19 +17,32 @@ function Loginscreen() {
             password,
         }
         try {
+
+            setloading(true);
             const result = await axios.post('/api/users/login', user).data
+            setloading(false);
+            localStorage.setItem('currentUser', JSON.stringify(result));
+
+            window.location.href = '/home'
+
         } catch (error) {
             console.log(error)
+            setloading(false);
+            seterror(true)
+
         }
-        console.log(user)
+
 
     }
 
 
     return (
         <div>
+            {loading && (<Loader />)}
+
             <div className="row justify-content-center mt-5">
                 <div className="col-md-5 mt-5">
+                    {error && (<Error message='Invalid Input' />)}
                     <div className='bs' >
                         <h2>Login</h2>
                         <input type="text" className="form-control" placeholder="email" value={email} onChange={(e) => { setemail(e.target.value) }} />
